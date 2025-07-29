0cbb6736357c40aa670a051c9fa9de3299f2a563
TonyzCataldo <antoniotonyzz71@gmail.com>
2025-07-29 20:03:22 -0300

    Header Input container create

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import Button from "../Button/Button";
+import Input from "../Input/Input";
 import * as S from "./styles";
 
 const Header = () => {
   return (
     <S.Header>
-      <Button>btn header</Button>
+      <S.InputContainer>
+        <Button>btn header</Button>
+        <Input />
+      </S.InputContainer>
     </S.Header>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,7 +2,16 @@ import styled from "styled-components";
 
 export const Header = styled.header`
   display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   background-color: ${(props) => props.theme.colors["dark-blue"]};
   color: ${(props) => props.theme.colors["neutral-200"]};
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 200px;
 `;
